77ba373ec771607c3efb58b9cb5975b5820116e1
Janou <janoutreffers@hotmail.com>
2023-06-26 16:44:37 +0200

    Animation progress

--- JS/main.js
+++ JS/main.js
@@ -1,61 +1,76 @@
-const navMenu = document.querySelector(".nav-menu");
-const hamburger = document.querySelector(".toggleBtn");
-const closeIcon = document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
-const navLink = document.querySelectorAll(".nav-link");
+/* eslint-disable no-undef */
+const navMenu = document.querySelector('.nav-menu');
+const hamburger = document.querySelector('.toggleBtn');
+const closeIcon = document.querySelector('.closeIcon');
+const menuIcon = document.querySelector('.menuIcon');
+const navLink = document.querySelectorAll('.nav-link');
 
 const toggleMenu = () => {
-    if (navMenu.classList.toggle("active")) {
-        closeIcon.style.display = "block";
-        menuIcon.style.display = "none";
-    } else {
-        closeMenu()
-    }
-}
-hamburger.addEventListener("click", toggleMenu);
+  if (navMenu.classList.toggle('active')) {
+    closeIcon.style.display = 'block';
+    menuIcon.style.display = 'none';
+  } else {
+    closeMenu();
+  }
+};
+hamburger.addEventListener('click', toggleMenu);
 
 const closeMenu = () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-}
-navLink.forEach((navLink) => navLink.addEventListener("click", closeMenu));
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+  closeIcon.style.display = 'none';
+  menuIcon.style.display = 'block';
+};
+navLink.forEach((navLink) => navLink.addEventListener('click', closeMenu));
 
 // GSAP Hero animation
-let images = document.querySelector('.images')
+let images = document.querySelector('.images');
 const janouImage = images.firstElementChild;
 const hertzImage = images.lastElementChild;
 
 let tl = gsap.timeline();
 
-gsap.to(".hero", { opacity: 1 });
-gsap.set(".janou", { opacity: 1 });
-gsap.timeline()
-    .from(janouImage, 1.2, { y: "80vh", opacity: 1, ease: "power3.out" }, "Start")
-    .from(janouImage.firstElementChild, 2, { scale: 1.6, ease: "power3.easeOut" }, .2)
-    .from(hertzImage, 1.4, { x: "80vh", opacity: 1, ease: "power3.easeOut" }, .2)
-    .from(hertzImage.lastElementChild, 2, { scale: 1.8, ease: "power3.out" }, .2)
+gsap.to('.hero', { opacity: 1 });
+gsap.set('.janou', { opacity: 1 });
+gsap
+  .timeline()
+  .from(janouImage, 1.2, { y: '80vh', opacity: 1, ease: 'power3.out' }, 'Start')
+  .from(
+    janouImage.firstElementChild,
+    2,
+    { scale: 1.6, ease: 'power3.easeOut' },
+    0.2
+  )
+  .from(hertzImage, 1.4, { x: '80vh', opacity: 1, ease: 'power3.easeOut' }, 0.2)
+  .from(
+    hertzImage.lastElementChild,
+    2,
+    { scale: 1.8, ease: 'power3.out' },
+    0.2
+  );
 
-let content = document.querySelector('.hero-content-inner')
+let content = document.querySelector('.hero-content-inner');
 
-const headlineFirst = content.children[0].children[0]
+const headlineFirst = content.children[0].children[0];
 const headlineSecond = content.children[0].children[1];
 const headlineThird = content.children[0].children[2];
-const contentP = content.children[1]
-const btnRow = content.children[2]
+const contentP = content.children[1];
+const btnRow = content.children[2];
 
-tl.staggerFrom([headlineFirst.children, headlineSecond.children, headlineThird.children],
-    1,
-    {
-        y: 60,
-        opacity: 0,
-        stagger: 0.3,
-        delay: .8
-    },
-    0.15, 'Start');
-tl.from(contentP, 1, { y: 20, opacity: 0, ease: "Power3.easeOut" }, 1.4)
-tl.from(btnRow, 1, { y: 20, opacity: 0, ease: "Power3.easeOut" }, 1.6)
+tl.staggerFrom(
+  [headlineFirst.children, headlineSecond.children, headlineThird.children],
+  1,
+  {
+    y: 60,
+    opacity: 0,
+    stagger: 0.3,
+    delay: 0.8,
+  },
+  0.15,
+  'Start'
+);
+tl.from(contentP, 1, { y: 20, opacity: 0, ease: 'Power3.easeOut' }, 1.4);
+tl.from(btnRow, 1, { y: 20, opacity: 0, ease: 'Power3.easeOut' }, 1.6);
 
 // // Cursor animation
 // const coords = { x: 0, y: 0 };
@@ -99,7 +114,3 @@ tl.from(btnRow, 1, { y: 20, opacity: 0, ease: "Power3.easeOut" }, 1.6)
 // }
 
 // animateCircles();
-
-
-
-
